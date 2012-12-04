922780f6e80d53adc0dc459b730deecc26c6d9ba
jeffreyhorner <jeffrey.horner@gmail.com>
2012-12-04 16:04:34 -0600

    Added spower results, more descriptions

--- server.R
+++ server.R
@@ -1,24 +1,53 @@
 library(shiny)
 library(Hmisc)
 
-rcens <- function(n) 1 + (5-1) * (runif(n) ^ .5)
-
 # Define server logic
 shinyServer(function(input, output) {
+   cat('shinyServer called\n',file=stderr())
 
-output$psPlot <- reactivePlot(function(){
-   p1 <- input$p1
-   p2 <- input$p2
-   mo <- input$mo
+   appData <- reactive(function() {
+      cat('appData called\n',file=stderr())
+      sink('/dev/null')
+      e <- new.env()
+      e$t1 <- input$t1
+      e$t2 <- input$t2
+      e$p1 <- input$p1
+      e$p2 <- input$p2
+      e$mo <- input$mo
+      e$nc <- input$nc
+      e$ni <- input$ni
+      e$nsim <- input$nsim
+      e$hr <- input$hr
+      e$sc <- Weibull2(c(e$t1,e$t2),c(e$p1,e$p2))
+      e$f <- Quantile2(e$sc,
+         hratio=function(x) ifelse(x <= e$mo/12, 1, e$hr),
+         dropin=function(x) ifelse(x <= .5, 0, .15 * (x-.5)/(5-.5)),
+         dropout=function(x) .3*x/5
+      )
+      sink()
 
-   sc <- Weibull2(c(1,3),c(p1,p2))
-   f <- Quantile2(sc,
-      hratio=function(x) ifelse(x <= mo/12, 1, .75),
-      dropin=function(x) ifelse(x <= .5, 0, .15 * (x-.5)/(5-.5)),
-      dropout=function(x) .3*x/5
-   )
-   par(mfrow=c(2,2))
-   plot(f,'all',label.curves=list(keys='lines'))
-})
+      e
+   })
+
+   output$psPlot <- reactivePlot(function(){
+      cat('output$psPlot called\n',file=stderr())
+      d <- appData()
+      par(mfrow=c(2,2))
+      plot(d$f,'all',label.curves=list(keys='lines'))
+   })
+
+   output$power <- reactivePrint(function(){
+      cat('output$power called\n',file=stderr())
+      with(appData(),{
+         sink('/dev/null')
+         rcens <- function(n) 1 + (5-1) * (runif(n) ^ .5)
+         rcontrol <- function(n) f(n,'control')
+         rinterv <- function(n) f(n,'intervention')
+         set.seed(211)
+         x <- spower(rcontrol,rinterv,rcens, nc=nc, ni=ni, test=logrank, nsim=nsim,cox=TRUE)
+         sink()
+         x
+      })
+   })
 
 })
--- ui.R
+++ ui.R
@@ -1,31 +1,56 @@
 library(shiny)
 
-# Define UI for miles per gallon application
+overview <- HTML("
+<p>
+We simulate a 2-arm
+5-year follow-up study using Hmisc. All subjects are followed
+at least one year, and patients enter the study with linearly
+increasing probability starting with zero. Assume (1) there is
+no chance of dropin for the first 6 months, then the probability
+increases linearly up to .15 at 5 years; and (2) there is a linearly
+increasing chance of dropout up to .3 at 5 years.
+</p>
+<h3>Instructions</h3>
+<ul>
+<li>Click on the <b>Design Displays</b> and <b>Results</b> tabs above to see graphics and spower results.
+</li>
+<li>Change any variable on the left and it will automatically update the tabs.</li>
+")
+
+customHeaderPanel <- function(title,windowTitle=title){
+    tagList(
+       tags$head(
+          tags$title(windowTitle),
+          tags$link(rel="stylesheet", type="text/css",
+                  href="app.css")
+       ), 
+       div(class = "span12", style = "padding: 10px 0px;", h1(title))
+    )
+}
+
 shinyUI(pageWithSidebar(
 
    # Application title
-   headerPanel("Power and Sample Size with spower from Hmisc"),
+   customHeaderPanel("Power and Sample Size with spower from Hmisc"),
 
    sidebarPanel(
-      numericInput("p1", "1-year Survival Rate:", .95,step=.01),
-      numericInput("p2", "3-year Survival Rate:", .7,step=.01),
+      numericInput("t1", "First Time Point (t1, years) ", 1,step=1),
+      numericInput("t2", "Second Time Point (t2, years) ", 3,step=1),
+      numericInput("p1", "t1-year Control Group Survival Prob.:", .95,step=.01),
+      numericInput("p2", "t2-year Control Group Survival Prob.:", .7,step=.01),
+      numericInput("hr", "Long-term Treatment Effect (hazard ratio):", .75, step=.05),
       numericInput("mo", "Treatment Effect Start Month:", 9,step=1),
+      numericInput("nc", "Number of Control Subjects:", 100, step=25),
+      numericInput("ni", "Number of Intervention Subjects:", 100, step=25),
+      numericInput("nsim", "Number of Simulated Trials:", 100, step=50)
 
-      helpText('This example demonstrates the flexibility of spower and
-      related functions from Hmisc. We simulate a 2-arm (350 subjects/arm)
-      5-year follow-up study for wich the control group\'s survival
-      distribution is Weibull with 1-year survival of .95 and 3-year
-      survival of .7. All subjects are followed at least one year,
-      and patients enter the study with linearly increasing probability
-      starting with zero. Assume (1) there is no chance of dropin for the
-      first 6 months, then the probability increases linearly up to .15
-      at 5 years; (2) there is a linearly increasing chance of dropout up
-      to .3 at 5 years; and (3) the treatment has no effect for the first
-      9 months, then it has a constant effect (hazard ratio of .75).'
-   )
-),
+   ),
 
    mainPanel(
-      plotOutput('psPlot',height='600px')
+      tabsetPanel(
+         tabPanel("Design Displays",plotOutput('psPlot',height='600px')),
+         tabPanel("Results",verbatimTextOutput("power")),
+         tabPanel("Overview",div(class="spower-overview",overview))
+      )
    )
 ))
